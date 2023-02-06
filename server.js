16f1d027880d5387c5e85c0077794983e3d52c19
soilihi laydi <soilihi.laydi@gmail.com>
2023-02-06 10:49:49 +0100

    creation des routes je n ai pas mis nodemon en depandance car il en global donc activation de nodemon

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express()
 
 const PORT = 4000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/api', (req, res) => {
+  res.send('Welcome to the basic the API Rest')
+})
+
+
+
+
 
 
 app.listen(PORT, () => {
